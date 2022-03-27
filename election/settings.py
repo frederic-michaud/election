f8f3595324b128873058ffb1b78de40e51f094f0
Frédéric Michaud <frederic.a.michaud@gmail.com>
2022-03-27 20:38:00 +0200

    Update config to use postgre server

--- election/settings.py
+++ election/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'election.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'votation',
+        'USER': 'laurence',
+        'PASSWORD': 'lau2006',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
