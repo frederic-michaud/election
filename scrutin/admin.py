ee98b4f183dd0517ff3f2209d7c1598bbb2d3d97
Frédéric Michaud <frederic.a.michaud@gmail.com>
2022-05-01 20:35:52 +0200

    Create database structure and import script for on-going votation

--- scrutin/admin.py
+++ scrutin/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import SujetVote, Commune, Voix, Canton, District
+from .models import SujetVote, Commune, Voix, Canton, District, ScrutinEnCours
 
 admin.site.register(SujetVote)
 admin.site.register(Commune)
 admin.site.register(Voix)
 admin.site.register(Canton)
 admin.site.register(District)
+admin.site.register(ScrutinEnCours)
