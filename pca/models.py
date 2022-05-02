95a3e1217c07578835e5711061c1065484f58f21
Frédéric Michaud <frederic.a.michaud@gmail.com>
2022-05-02 20:53:33 +0200

    Add more components to the PCA for extrapolation

--- pca/models.py
+++ pca/models.py
@@ -6,5 +6,9 @@ class PCAResult(models.Model):
     commune = models.ForeignKey(Commune, on_delete=models.CASCADE)
     coordinate_1 = models.FloatField()
     coordinate_2 = models.FloatField()
+    coordinate_3 = models.FloatField()
+    coordinate_4 = models.FloatField()
+    coordinate_5 = models.FloatField()
+    coordinate_6 = models.FloatField()
     def  __str__(self):
         return str(self.commune)
