ec74bc097e26e1d21b5aee68d7d2c3963e18d5fd
Frédéric Michaud <frederic.a.michaud@gmail.com>
2022-09-14 21:28:44 +0200

    Increase speed of extrapolation

--- scrutin/extrapolation.py
+++ scrutin/extrapolation.py
@@ -7,16 +7,22 @@ nb_component = 6
 def get_percentage(component, params):
     return np.sum(component * params[:-1]) + params[-1]
 
-def Delta(params, data_all_commune):
-    extrapolation = [get_percentage(component, params) for _, component, _ in data_all_commune]
-    observed = [percentage for percentage, _, _ in data_all_commune]
-    nb_votants = [nb_votant for _, _, nb_votant in data_all_commune]
+def Delta(params, components, observed, nb_votants):
+    extrapolation = [get_percentage(component, params) for component in components]
     return np.sum(np.square(np.array(extrapolation) - np.array(observed))*nb_votants)
+
+def Delta_fast(params, components, observed, nb_votants):
+    extrapolation = np.sum(components*params[:-1], axis = 1) + params[-1]
+    return np.sum(np.square(np.array(extrapolation) - np.array(observed))*nb_votants)
+
 
 def get_linear_parameter(data_all_commune):
     x_init = np.full(nb_component + 1, 0.)
     x_init[-1] = 0.5
-    extrapolated_param = scipy.optimize.minimize(Delta, x_init, args=data_all_commune)
+    components = [component for _, component, _ in data_all_commune]
+    observed = [percentage for percentage, _, _ in data_all_commune]
+    nb_votants = [nb_votant for _, _, nb_votant in data_all_commune]
+    extrapolated_param = scipy.optimize.minimize(Delta_fast, x_init, args=(components, observed, nb_votants))
     return extrapolated_param.x
 
 def get_extrapolated_value(components, params):
@@ -31,11 +37,15 @@ def get_extrapolation(sujet):
     data_for_interpolating_participation = []
     nbre_votant_approximated = []
     commune_without_result = []
+    PCAResult_dict = {
+        pca_result.commune_id: pca_result
+        for pca_result in PCAResult.objects.all()
+    }
     for voix in ScrutinEnCours.objects.filter(sujet_vote = sujet).order_by("commune"):
-        pca = PCAResult.objects.filter(commune=voix.commune)
-        if (len(pca) != 1):
+        try:
+            pca = PCAResult_dict[voix.commune_id]
+        except:
             raise Exception('Commune pca not found')
-        pca = pca[0]
         if voix.comptabilise:
             data_for_interpolating_pourcentage_oui.append((voix.get_pourcentage_oui(), pca.get_component(nb_component), voix.bulletins_rentres))
             data_for_interpolating_participation.append((voix.get_real_participation(), pca.get_component(nb_component), voix.bulletins_rentres))
@@ -47,7 +57,6 @@ def get_extrapolation(sujet):
             commune_without_result.append(voix)
     if len(data_for_interpolating_participation) < 7:
         return 0.5, 0.5, 0, [], [], []
-
     nbre_votant_approximated = np.array(nbre_votant_approximated)
     params_pourcentage_oui = get_linear_parameter(data_for_interpolating_pourcentage_oui)
     params_participation = get_linear_parameter(data_for_interpolating_participation)
