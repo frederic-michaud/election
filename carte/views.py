e7c97a10071fc88c4e1cc2bd408d601b3e8d2f54
Frédéric Michaud <frederic.a.michaud@gmail.com>
2022-10-09 16:46:20 +0200

    improve map design

--- carte/views.py
+++ carte/views.py
@@ -9,27 +9,37 @@ def carte_view(requete, *args, **kwargs):
     with open("data/switzerland2.geojson") as f:
         gj = geojson.load(f)
     all_cities = []
-    all_cities_id = []
-    for entry in gj["features"]:
-        all_cities.append(entry["properties"]['NAME'])
-        all_cities_id.append(entry["properties"]['BFS_NUMMER'])
+    all_results = []
     oui_per_commune_id = ScrutinAPI.get_percentage_oui_all_commune(6)
-    d = {'name' : all_cities,
-         'results':list(map(lambda x: oui_per_commune_id.get(x, 0), all_cities_id))}
-    array_for_color = np.array(d['results'])
+    for entry in gj["features"]:
+        if entry['properties']['BFS_NUMMER'] in oui_per_commune_id.keys():
+            all_cities.append(entry["properties"]['NAME'])
+            all_results.append(oui_per_commune_id[entry['properties']['BFS_NUMMER']]*100)
+    all_results_formated = list(map(lambda x: f'{x:.2f} %', all_results))
+    dict_properties = {'name': all_cities,
+                       'results': all_results,
+                       'results_formated': all_results_formated}
+    array_for_color = np.array(dict_properties['results'])
     array_for_color = array_for_color[array_for_color > 0]
-    lower_bound_color = np.percentile(array_for_color,10)
-    upper_bound_color = np.percentile(array_for_color,90)
-    a = plotly.offline.plot(px.choropleth_mapbox(d, geojson=gj, locations='name', color='results',
-                           color_continuous_scale="Viridis",
-                           featureidkey="properties.NAME",
-                           range_color=(lower_bound_color, upper_bound_color),
-                           mapbox_style="carto-positron",
-                           zoom=3,
-                           opacity=0.5,
-                           labels={'results':'Resultat', 'name' : 'Nom'}
-                          ),
-                            include_plotlyjs=False,
+    lower_bound_color = np.percentile(array_for_color, 10)
+    upper_bound_color = np.percentile(array_for_color, 90)
+    div_containing_plot = plotly.offline.plot(px.choropleth_mapbox(dict_properties,
+                                                                   geojson=gj,
+                                                                   locations='name',
+                                                                   color='results',
+                                                                   center={"lat": 46.92, "lon": 8.22},
+                                                                   zoom=6,
+                                                                   # 20 is extremly zoomed... 10 still too much. 7 slightly too much
+                                                                   color_continuous_scale="RdYlGn",
+                                                                   featureidkey="properties.NAME",
+                                                                   range_color=(lower_bound_color, upper_bound_color),
+                                                                   mapbox_style="white-bg",
+                                                                   opacity=0.5,
+                                                                   labels={'results_formated': 'Resultat',
+                                                                           'name': 'Nom'},
+                                                                   hover_data={'name': True, 'results_formated': True,
+                                                                               'results': False}
+                                                                   ),
+                                              include_plotlyjs=False,
                             output_type='div')
-
-    return render(requete, "home.html", {'plot':a})
+    return render(requete, "home.html", {'plot': div_containing_plot})
